cc4f9deb8c9baae1547e514831f146ffe5dde52c
jhayboy <mbamarajustice1@gmail.com>
2022-06-21 22:45:01 +0100

    update

--- comJS/shopit.js
+++ comJS/shopit.js
@@ -3,13 +3,19 @@ const navList = document.querySelector('.nav-list')
 const navBtn = document.querySelector('.nav-btn')
 const liLi = document.querySelectorAll('.li')
 const upper = document.querySelector('.upper')
-
+const shopitNav = document.querySelector('.shopit-nav')
 
 window.onscroll = () => {
     if (window.scrollY > 3000) {
         upper.classList.add("upping")
     } else {
         upper.classList.remove("upping")
+    }
+
+    if (window.scrollY > 250) {
+        shopitNav.classList.add("shopit-color")
+    } else {
+        shopitNav.classList.remove("shopit-color")
     }
 }
 hamBurger.addEventListener('click', () => {
